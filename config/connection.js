12f895be183fd6df215e29bc27e6d9937886835a
cjamilc <castro.celioj@gmail.com>
2018-09-23 10:55:35 -0400

    environment database

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "roots",
-    database: "burgers_db"
-  });
+    var connection;
 
-connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
+    if (process.env.JAWSDB_URL) {
+        connection= mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+        connection = mysql.createConnection({
+            host:"localhost",
+            port: 3306,
+            user: "root",
+            password: "roots",
+            database: "burgers_db"
+        });
+    };
+
+    connection.connect((err) => {
+        if (err) {
+            console.error("error connecting: " + err.stack);
         return;
     }
     console.log("connected as id " + connection.threadId);
